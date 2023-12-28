bbf9fd396ddde69fb3cb0fae9228de26012ab37e
Vlxdi <vladipetrov006@gmail.com>
2023-12-28 17:17:44 +0200

    Add project files.

---
+++ OnlineEducationalPlatformNEW/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineEducationalPlatformNEW.Models;
+
+namespace OnlineEducationalPlatformNEW.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
---
+++ OnlineEducationalPlatformNEW/Models/Courses.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineEducationalPlatformNEW.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string CourseId { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = string.Empty;
+    }
+}
